7b8743cb7b1d96d11968b2968e253301750479d0
Heinner Vega <heinnervega20@gmail.com>
2023-08-27 00:24:19 -0500

    HU03 Add RoomModel y RoomRepository

---
+++ hotel-service/src/main/java/com/playko/hotelservice/model/RoomModel.java
@@ -0,0 +1,70 @@
+package com.playko.hotelservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "room-table")
+public class RoomModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String type;
+    private String available;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id") // Nombre de la columna en la tabla de habitaciones que hace referencia al hotel
+    private HotelModel hotel;
+
+
+    public RoomModel() {
+    }
+
+    public RoomModel(Long id, String type, String available, HotelModel hotel) {
+        this.id = id;
+        this.type = type;
+        this.available = available;
+        this.hotel = hotel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(String available) {
+        this.available = available;
+    }
+
+    public HotelModel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(HotelModel hotel) {
+        this.hotel = hotel;
+    }
+}
