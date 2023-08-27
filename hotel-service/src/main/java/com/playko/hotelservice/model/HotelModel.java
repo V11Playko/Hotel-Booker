60ef1caa76f93bdd4c2ec6cded992419d6a0a056
Heinner Vega <heinnervega20@gmail.com>
2023-08-27 00:27:12 -0500

    HU03 Change saveHotel method to save the corresponding rooms

--- hotel-service/src/main/java/com/playko/hotelservice/model/HotelModel.java
+++ hotel-service/src/main/java/com/playko/hotelservice/model/HotelModel.java
@@ -1,12 +1,17 @@
 package com.playko.hotelservice.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Table(name = "hotel-table")
@@ -18,15 +23,20 @@ public class HotelModel implements Serializable {
     private Long id;
     private String name;
     private String address;
-    private String starsCategory;
+    @Min(value = 1, message = "La categoría de estrellas debe ser un número entre 1 y 5.")
+    @Max(value = 5, message = "La categoría de estrellas debe ser un número entre 1 y 5.")
+    private Integer starsCategory;
     private Integer numberRooms;
     private String facilities;
     private Double averagePricePerNight;
 
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL)
+    private List<RoomModel> rooms;
+
     public HotelModel() {
     }
 
-    public HotelModel(Long id, String name, String address, String starsCategory, Integer numberRooms, String facilities, Double averagePricePerNight) {
+    public HotelModel(Long id, String name, String address, Integer starsCategory, Integer numberRooms, String facilities, Double averagePricePerNight, List<RoomModel> rooms) {
         this.id = id;
         this.name = name;
         this.address = address;
@@ -34,6 +44,7 @@ public class HotelModel implements Serializable {
         this.numberRooms = numberRooms;
         this.facilities = facilities;
         this.averagePricePerNight = averagePricePerNight;
+        this.rooms = rooms;
     }
 
     public Long getId() {
@@ -60,11 +71,11 @@ public class HotelModel implements Serializable {
         this.address = address;
     }
 
-    public String getStarsCategory() {
+    public Integer getStarsCategory() {
         return starsCategory;
     }
 
-    public void setStarsCategory(String starsCategory) {
+    public void setStarsCategory(Integer starsCategory) {
         this.starsCategory = starsCategory;
     }
 
@@ -90,5 +101,13 @@ public class HotelModel implements Serializable {
 
     public void setAveragePricePerNight(Double averagePricePerNight) {
         this.averagePricePerNight = averagePricePerNight;
+    }
+
+    public List<RoomModel> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<RoomModel> rooms) {
+        this.rooms = rooms;
     }
 }
--- hotel-service/src/main/java/com/playko/hotelservice/service/impl/HotelService.java
+++ hotel-service/src/main/java/com/playko/hotelservice/service/impl/HotelService.java
@@ -1,7 +1,9 @@
 package com.playko.hotelservice.service.impl;
 
 import com.playko.hotelservice.model.HotelModel;
+import com.playko.hotelservice.model.RoomModel;
 import com.playko.hotelservice.repository.IHotelRepository;
+import com.playko.hotelservice.repository.IRoomRepository;
 import com.playko.hotelservice.service.IHotelService;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,76 @@ import org.springframework.stereotype.Service;
 public class HotelService implements IHotelService {
 
     private final IHotelRepository hotelRepository;
+    private final IRoomRepository roomRepository;
 
-    public HotelService(IHotelRepository hotelRepository) {
+    public HotelService(IHotelRepository hotelRepository, IRoomRepository roomRepository) {
         this.hotelRepository = hotelRepository;
+        this.roomRepository = roomRepository;
     }
 
     @Override
     public void saveHotel(HotelModel hotelModel) {
+        // Guarda el hotel en la base de datos
         hotelRepository.save(hotelModel);
+
+        // Define las proporciones de habitaciones según la categoría de estrellas
+        double[] roomProportions = getRoomProportions(hotelModel.getStarsCategory(), hotelModel.getNumberRooms());
+
+        // Define las categorías de habitaciones y sus cantidades
+        String[] roomCategories = {"Standard", "Superior", "Suite"};
+        int[] roomCounts = new int[roomCategories.length];
+
+        // Calcula la cantidad de habitaciones para cada categoría
+        for (int i = 0; i < roomCategories.length; i++) {
+            roomCounts[i] = (int) (roomProportions[i] * hotelModel.getNumberRooms());
+        }
+
+        // Crea y guarda las habitaciones en la base de datos
+        for (int i = 0; i < roomCategories.length; i++) {
+            for (int j = 0; j < roomCounts[i]; j++) {
+                RoomModel room = new RoomModel();
+                room.setType(roomCategories[i]);
+                room.setAvailable("Yes");
+                room.setHotel(hotelModel);
+                roomRepository.save(room);
+            }
+        }
     }
+
+    private double[] getRoomProportions(Integer starsCategory, Integer numRooms) {
+        double[] proportions = {0.0, 0.0, 0.0}; // Inicializa todas las proporciones a 0
+
+        if (starsCategory != null && numRooms != null && numRooms > 0) {
+            switch (starsCategory) {
+                case 1:
+                    proportions[0] = 1.0; // 100% Standard
+                    break;
+                case 2:
+                    proportions[0] = 0.7; // 70% Standard
+                    proportions[1] = 0.3; // 30% Superior
+                    break;
+                case 3:
+                    proportions[0] = 0.6; // 60% Standard
+                    proportions[1] = 0.3; // 30% Superior
+                    proportions[2] = 0.1; // 10% Suite
+                    break;
+                case 4:
+                    proportions[0] = 0.5; // 50% Standard
+                    proportions[1] = 0.4; // 40% Superior
+                    proportions[2] = 0.1; // 10% Suite
+                    break;
+                case 5:
+                    proportions[0] = 0.4; // 40% Standard
+                    proportions[1] = 0.4; // 40% Superior
+                    proportions[2] = 0.2; // 20% Suite
+                    break;
+                default:
+                    // Tratar otros valores de starsCategory si es necesario
+                    break;
+            }
+        }
+
+        return proportions;
+    }
+
 }
