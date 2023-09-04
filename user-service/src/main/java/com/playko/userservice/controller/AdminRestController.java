5c16c4db528b4995270600a020a63d6f93150509
Heinner Vega <heinnervega20@gmail.com>
2023-09-04 09:37:46 -0500

    HU06 Add dto and mapper for user

---
+++ user-service/src/main/java/com/playko/userservice/controller/AdminRestController.java
@@ -0,0 +1,67 @@
+package com.playko.userservice.controller;
+
+
+import com.playko.userservice.configuration.Constants;
+import com.playko.userservice.model.UserModel;
+import com.playko.userservice.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users/v1/admin")
+public class UserRestController {
+    private final IUserService userService;
+
+    public UserRestController(IUserService userService) {
+        this.userService = userService;
+    }
+
+
+    @Operation(summary = "Add a new user",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "User created",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Map"))),
+                    @ApiResponse(responseCode = "409", description = "User already exists",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error"))),
+                    @ApiResponse(responseCode = "403", description = "Role not allowed for user creation",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
+    @PostMapping("/save-admin")
+    public ResponseEntity<Map<String, String>> saveAdmin(@Valid @RequestBody UserModel user) {
+        userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_CREATED_MESSAGE));
+        }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/save-owner")
+    public ResponseEntity<Map<String, String>> saveOwner(@Valid @RequestBody UserModel user) {
+        userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_CREATED_MESSAGE));
+    }
+    @PreAuthorize("hasRole('OWNER')")
+    @PostMapping("/save-employee")
+    public ResponseEntity<Map<String, String>> saveEmployee(@Valid @RequestBody UserModel user) {
+        userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_CREATED_MESSAGE));
+    }
+    @PostMapping("/save-client")
+    public ResponseEntity<Map<String, String>> saveClient(@Valid @RequestBody UserModel user) {
+        userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_CREATED_MESSAGE));
+    }
+}
