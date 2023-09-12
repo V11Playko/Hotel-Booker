618e43804c769f33e118b3a5b797a0b53b70e735
Heinner Vega <heinnervega20@gmail.com>
2023-09-12 11:03:38 -0500

    HU07 Add Feign Client

--- user-service/src/main/java/com/playko/userservice/repository/IUserRepository.java
+++ user-service/src/main/java/com/playko/userservice/repository/IUserRepository.java
@@ -4,6 +4,9 @@ package com.playko.userservice.repository;
 import com.playko.userservice.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUserRepository extends JpaRepository<UserModel, Long> {
     UserModel findByEmail(String email);
+    Optional<UserModel> findByIdAndRoleId(Long idUser, Long idRole);
 }
