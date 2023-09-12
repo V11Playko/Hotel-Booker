98ec9df3b06a983c08b9a666a11582c449f10259
Heinner Vega <heinnervega20@gmail.com>
2023-09-12 08:15:43 -0500

    HU07 Add new methods to call users by their roles

--- user-service/src/main/java/com/playko/userservice/controller/EmployeeRestController.java
+++ user-service/src/main/java/com/playko/userservice/controller/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.playko.userservice.controller;
 import com.playko.userservice.configuration.Constants;
 import com.playko.userservice.controller.dto.UserRequestDto;
 import com.playko.userservice.controller.mapper.IUserRequestMapper;
+import com.playko.userservice.model.UserModel;
 import com.playko.userservice.service.IUserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,6 +13,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users/v1/employee")
@@ -32,4 +36,14 @@ public class EmployeeRestController {
         this.userRequestMapper = userRequestMapper;
     }
 
+    @Operation(summary = "Get a user with role CLIENT",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User returned",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserRequestDto.class))),
+                    @ApiResponse(responseCode = "404", description = "User not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
+    @GetMapping("/getClient/{id}")
+    public ResponseEntity<Optional<UserModel>> getClient(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getClient(id));
+    }
 }
