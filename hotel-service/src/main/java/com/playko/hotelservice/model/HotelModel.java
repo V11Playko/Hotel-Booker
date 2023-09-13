336b03eaf2aa6ed7b592f0f69ad291ccce9f59c3
Heinner Vega <heinnervega20@gmail.com>
2023-09-13 14:58:56 -0500

    HU07 Change getter and seter in model

--- hotel-service/src/main/java/com/playko/hotelservice/model/HotelModel.java
+++ hotel-service/src/main/java/com/playko/hotelservice/model/HotelModel.java
@@ -11,12 +11,16 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Entity
-@Table(name = "hotel-table")
+@Table(name = "hotel-tb")
+@Getter
+@Setter
 public class HotelModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -38,80 +42,5 @@ public class HotelModel implements Serializable {
     private List<RoomModel> rooms;
 
     public HotelModel() {
-    }
-
-    public HotelModel(Long id, String name, String address, Integer starsCategory, Integer numberRooms, String facilities, Double averagePricePerNight, List<RoomModel> rooms) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.starsCategory = starsCategory;
-        this.numberRooms = numberRooms;
-        this.facilities = facilities;
-        this.averagePricePerNight = averagePricePerNight;
-        this.rooms = rooms;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Integer getStarsCategory() {
-        return starsCategory;
-    }
-
-    public void setStarsCategory(Integer starsCategory) {
-        this.starsCategory = starsCategory;
-    }
-
-    public Integer getNumberRooms() {
-        return numberRooms;
-    }
-
-    public void setNumberRooms(Integer numberRooms) {
-        this.numberRooms = numberRooms;
-    }
-
-    public String getFacilities() {
-        return facilities;
-    }
-
-    public void setFacilities(String facilities) {
-        this.facilities = facilities;
-    }
-
-    public Double getAveragePricePerNight() {
-        return averagePricePerNight;
-    }
-
-    public void setAveragePricePerNight(Double averagePricePerNight) {
-        this.averagePricePerNight = averagePricePerNight;
-    }
-
-    public List<RoomModel> getRooms() {
-        return rooms;
-    }
-
-    public void setRooms(List<RoomModel> rooms) {
-        this.rooms = rooms;
     }
 }
