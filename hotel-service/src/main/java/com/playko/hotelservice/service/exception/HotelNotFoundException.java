a13e8f3b84a9a658bfa768b14d2008c869129a8f
Heinner Vega <heinnervega20@gmail.com>
2023-09-14 08:31:54 -0500

    feature/manejo-de-excepciones Add three new exceptions

---
+++ hotel-service/src/main/java/com/playko/hotelservice/service/exception/HotelNotFoundException.java
@@ -0,0 +1,7 @@
+package com.playko.hotelservice.service.exception;
+
+public class HotelNotFoundException extends RuntimeException{
+    public HotelNotFoundException() {
+        super();
+    }
+}
---
+++ hotel-service/src/main/java/com/playko/hotelservice/service/exception/RoomUnavailableException.java
@@ -0,0 +1,5 @@
+package com.playko.hotelservice.service.exception;
+
+public class RoomUnavailableException extends RuntimeException{
+    public RoomUnavailableException() {super();}
+}
