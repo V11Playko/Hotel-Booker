d4c37301b04b3d504208f9179025c84373f55022
Heinner Vega <heinnervega20@gmail.com>
2023-10-09 07:12:44 -0500

    HU08 Change AuthorizationFilter to allow documentation

--- hotel-service/src/main/java/com/playko/hotelservice/configuration/security/jwt/JwtAuthorizationFilter.java
+++ hotel-service/src/main/java/com/playko/hotelservice/configuration/security/jwt/JwtAuthorizationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -20,6 +21,10 @@ import java.util.regex.Pattern;
 @RequiredArgsConstructor
 @Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private List<String> excludedPrefixes = Arrays.asList("/swagger-ui/**", "/v3/api-docs/**");
+    private AntPathMatcher pathMatcher = new AntPathMatcher();
+
 
     private boolean isEndpointMatch(String allowedEndpoint, String actualEndpoint) {
         String regex = allowedEndpoint
@@ -82,6 +87,17 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
         }
 
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String currentRoute = request.getServletPath();
+        for (String prefix : excludedPrefixes) {
+            if (pathMatcher.match(prefix, currentRoute)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private String getToken(HttpServletRequest request) {
