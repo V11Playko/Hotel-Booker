4dded789304b0749907d77f6bd8c4f8c34f4a62a
Heinner Vega <heinnervega20@gmail.com>
2024-02-05 09:08:37 -0500

    Use SonarLint to remove some unnecessary things

--- hotel-service/src/main/java/com/playko/hotelservice/model/ReservationModel.java
+++ hotel-service/src/main/java/com/playko/hotelservice/model/ReservationModel.java
@@ -9,17 +9,13 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
